2aac4a196e39fba2b3cc7d33457f71b4c1446a3e
preeti54 <pdhamija24@gmail.com>
2014-12-15 09:45:48 +0530

    button onclick

--- src/com/example/eventhandling/MainActivity.java
+++ src/com/example/eventhandling/MainActivity.java
@@ -1,23 +1,28 @@
 package com.example.eventhandling;
 
-import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
 
 public class MainActivity extends Activity {
-
+ private Button btnclick;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-    
+        
+        btnclick=(Button)findViewById(R.id.btnclick);
+        OnClickListener listener = new OnClickListener() {
+			
+			@Override
+			public void onClick(View arg0) {
+			btnclick.setText("Button was clicked");
+				
+			}
+		};
+		btnclick.setOnClickListener(listener);
+    }  
 }
