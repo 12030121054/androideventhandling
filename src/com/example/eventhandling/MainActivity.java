61477991b412510b9845ce5f32119e7a12e31d6d
preeti54 <pdhamija24@gmail.com>
2014-12-27 08:40:48 +0530

    open

--- src/com/example/eventhandling/MainActivity.java
+++ src/com/example/eventhandling/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.eventhandling;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,22 +10,24 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 
-public class MainActivity extends Activity {
+@SuppressLint("DefaultLocale") public class MainActivity extends Activity {
  private Button btnclick;
  private EditText text;
+ private Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         
         btnclick=(Button)findViewById(R.id.btnclick);
+        btn=(Button)findViewById(R.id.btn);
         text=(EditText)findViewById(R.id.text);
         OnClickListener listener = new OnClickListener() {
 			
 			@Override
 			
 			public void onClick(View v) { 
-			Toast.makeText(getApplicationContext(),text.getText(),Toast.LENGTH_LONG).show();
+			Toast.makeText(getApplicationContext(),text.toString().toLowerCase(),Toast.LENGTH_LONG).show();
 				
 			}
 		};
